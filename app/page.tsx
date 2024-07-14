ed07c1e1ecf02106aac87a4f65f3dfb52c228084
James Brown <soylentblue@hotmail.co.uk>
2024-07-14 23:03:59 +0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import { auth } from "@/auth"
 import { Chat } from "@/components/chat/chat"
 import { getAgent } from "@/lib/actions/db"
 import type { ChatPageProps } from "@/lib/types"
+import { toast } from "sonner"
 
 export default async function ChatPage({ searchParams }: ChatPageProps) {
   const agentId = searchParams?.a as string
   const agent = (agentId && (await getAgent(agentId))) || undefined
   const session = (await auth()) || undefined
+
+  toast.loading("ChatPage: " + agentId + " : " + agent + " : " + session)
+  console.log("ChatPage: " + agentId + " : " + agent + " : " + session);
 
   return <Chat agent={agent} session={session} />
 }
--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -14,6 +14,7 @@ import { DEFAULT_AGENT } from "@/app/config"
 import type { Agent } from "@/lib/types"
 import { cn } from "@/lib/utils"
 import { AgentCard } from "@/components/agent-card"
+import { toast } from "sonner"
 
 type ChatProps = {
   className?: string
